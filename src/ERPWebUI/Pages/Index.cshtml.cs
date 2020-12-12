d62f5ceaf1781adc3e5008dd463a800f2d27c158
Daniel Marques <dmarques.bh@gmail.com>
2020-12-12 16:02:21 -0300

    Build funcionando com acesso ao banco pelo DADOS

---
+++ src/ERPWebUI/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DADOS.OBJETOS;
+using Framework.Utilidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ERPWebUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var pXML = new ParametrosXml
+            {
+                { "ID_STATUS", 3 }
+            };
+            string innerXml = pXML.ToXML().InnerXml;
+
+            var atendimento = new Atendimento(4029993);
+            ViewData["Paciente"] = atendimento.PESSOA.NM_PESSOA;
+        }
+    }
+}
---
+++ src/Framework/Enum/eTipoDiversosFramework.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.CSharp;
+
+namespace Framework.Enum
+{
+    /// <summary>
+    /// Enum que representa TipoDiversos.
+    /// </summary>
+    public enum eTipoDiversos
+    {
+        Domingo = 2929,
+        Segundafeira = 2930,
+        TercaFeira = 2931,
+        QuartaFeira = 2932,
+        QuintaFeira = 2933,
+        SextaFeira = 2934,
+        Sabado = 2935,
+    }
+}
